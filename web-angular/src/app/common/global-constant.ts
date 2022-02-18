0cf905039177434ab40ec7f007de1227f15ff63b
shoy777 <froilan1512@gmail.com>
2022-02-18 05:32:55 -0500

    gitignore

--- web-angular/src/app/common/global-constant.ts
+++ web-angular/src/app/common/global-constant.ts
@@ -2,5 +2,6 @@ export class GlobalConstants{
     //https://www.itsolutionstuff.com/post/how-to-declare-global-variable-in-angular-8example.html
     public static siteWebName: string = "Froilan Cerna";
     public static subTitle: string = "Desarrollador Web";
-    public static slogan: string = "C#, JavasSript, CSS3, HTML5, Laravel, entre otros";
+    public static slogan: string = "C#, JavasSript, CSS3, HTML5, Laravel, entre otros sss";
+    public static abc = ""
 }
